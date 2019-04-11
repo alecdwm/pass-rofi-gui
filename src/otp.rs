87ef11a0c99e97040507c75a61649e21c2b3bed0
alecdwm <alec@owls.io>
2019-04-12 03:05:54 +1000

    switched from boringauth to libreauth for totp
    viral GPLv2.0 -> non-viral CeCILL-C

--- src/otp.rs
+++ src/otp.rs
@@ -1,11 +1,11 @@
-use boringauth::oath::TOTPBuilder;
 use failure::format_err;
 use failure::Error;
+use libreauth::oath::TOTPBuilder;
 
 pub fn calculate_otp(secret: &str) -> Result<String, Error> {
     Ok(TOTPBuilder::new()
         .base32_key(&secret.to_owned())
         .finalize()
-        .map_err(|err| format_err!("BoringAuth error: {:?}", err))?
+        .map_err(|err| format_err!("libreauth error: {:?}", err))?
         .generate())
 }
